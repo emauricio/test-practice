0f26906290ab304c84fb0179d9e9cec29fc16f35
Efren Mauricio <efren.mauricio@gmail.com>
2024-11-12 22:17:07 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # test-practice-2024
+
+bun no not require ts-node, so is more simple to play around.
+
+## tools
+
+- [code-runner-vscode](https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner)
+- [bun](https://bun.sh)
+- [vite](https://vite.dev/)
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  arrowParens: 'always',
+}
+
+export default config
